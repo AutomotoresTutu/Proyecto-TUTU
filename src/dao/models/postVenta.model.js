5632d70ba68fb27eb1796efd8d78bf4b7b428361
Tutu Automotores <automotorestutu4@gmail.com>
2024-04-11 10:36:42 -0300

    se agrega postventa

--- src/dao/models/postVenta.model.js
+++ src/dao/models/postVenta.model.js
@@ -1,3 +1,23 @@
 import mongoose from 'mongoose';
-const { Schema } = mongoose;
+
+const postventaSchema = new mongoose.Schema({
+    auto: { type: mongoose.Schema.Types.ObjectId, ref: 'Auto', required: true },
+    situacionPostventa: { type: String, required: true },
+    diagnosticoTaller: { type: String, required: true },
+    fechaRecepcion: { type: Date, required: true, default: Date.now },
+    fechaRetiro: { type: Date },
+    cantidadDiasTaller: { 
+        type: Number, 
+        default: function() {
+            if (this.fechaRetiro) {
+                return Math.ceil((this.fechaRetiro - this.fechaRecepcion) / (1000 * 60 * 60 * 24));
+            } else {
+                return Math.ceil((Date.now() - this.fechaRecepcion) / (1000 * 60 * 60 * 24));
+            }
+        }
+    }
+});
+
+const Postventa = mongoose.model('Postventa', postventaSchema);
+export default Postventa;
 
